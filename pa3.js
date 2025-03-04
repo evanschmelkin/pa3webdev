67a5b73debaeb172589ce2f1c1264f6571ef8f51
immersion-max <mdavis7@conncoll.edu>
2025-03-03 20:11:49 -0500

    max's first commit to this project

--- pa3.js
+++ pa3.js
@@ -1,3 +1,29 @@
-$('p').on('click', function() {
-    console.log('hi');
+nums = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+var counter = 0;
+var autoFlag = false;
+$('button').on('click', function(){
+    counter = $('form').val();
+    if (counter in nums){
+        if(autoFlag){
+            autoFlag = false;
+            clearTimeout(timer);
+        }
+        else{
+            autoFlag = true;
+            autoCount( );
+        }
+
+    }
 });
+
+
+function autoCount( ){
+    counter = (counter+1)%maxInd;
+    document.getElementById("counterText").innerHTML = myList[counter];
+    if(autoFlag)
+        timer = setTimeout("autoCount()",700);
+}
+
+
+
+
