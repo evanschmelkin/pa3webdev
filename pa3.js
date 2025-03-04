fcd16c267e581e6619174f03aa4ac278b1175fd7
evanschmelkin <evanschmelkin@gmail.com>
2025-03-03 22:06:26 -0500

    pa3 march 3rd all work togehER!

--- pa3.js
+++ pa3.js
@@ -1,28 +1,72 @@
-nums = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
-var counter = 0;
-var autoFlag = false;
-$('button').on('click', function(){
-    counter = $('form').val();
-    if (counter in nums){
-        if(autoFlag){
-            autoFlag = false;
-            clearTimeout(timer);
+$(document).ready(function(){
+
+    var input = 15;
+    var table = $('<table>').addClass('mul_table');
+
+
+    for (var i = 0; i <= input; i++) {
+        var row = $('<tr>').addClass('bar');
+        row.append($('<td>').text(i));
+        if (i === 0) {
+            //row.append($('<td>').text('hi'));
+            for (var j = 1; j <= input; j++) {
+                row.append($('<td>').text(j));
+            }
         }
-        else{
-            autoFlag = true;
-            autoCount( );
+        else {
+            for (var j = 1; j <= input; j++) {
+                row.append($('<td>').text(i*j));
+            }
         }
+
+        //if i === 0,
+        // add empty cell
+        //else:
+        //  do what you gotta do
+        table.append(row);
+    }
+
+
+    $('#here_table').append(table);
+
+    nums = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+    var counter = 10;
+    var autoFlag = false;
+    var timer;
+    $("button").on("click", function(){
+        counter = $("input").val();
+        if (counter in nums){
+            if(autoFlag){
+                autoFlag = false;
+                clearTimeout(timer);
+            }
+            else{
+                autoFlag = true;
+                autoCount();
+            }
+
+        }
+    });
+
+
+    function autoCount(){
+        if (counter >0){
+            console.log("test");
+            counter = counter-1;
+            $("p").html("timer left: " + counter); //the timer text
+            if(autoFlag)
+                timer = setTimeout(autoCount,1000);
+        }
+
+
+
 
     }
-});
 
 
-function autoCount( ){
-    counter = (counter+1)%maxInd;
-    document.getElementById("counterText").innerHTML = myList[counter];
-    if(autoFlag)
-        timer = setTimeout("autoCount()",700);
-}
+
+
+})
 
 
 
