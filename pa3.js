e69aa09b92115536543f9ea2f90fd3a2c2a2b73d
evanschmelkin <evanschmelkin@gmail.com>
2025-03-26 10:10:24 -0400

    mar 26 work together

--- pa3.js
+++ pa3.js
@@ -1,3 +1,4 @@
+//js
 /*
 Sa'ada, Max, and Evan
 3/4/2025
@@ -5,84 +6,122 @@ Web Development
 pa3.js
 This is the main JavaScript file for this project
 */
-$(document).ready(function(){
 
-    var input = 15;
-    var table = $('<table>').addClass('mul_table');
-    var counter = 1;
+var userInput;
 
-    for (var i = 0; i <= input; i++) { //rows (i) starts a 0
-
-        var row = $('<tr>').addClass('cell1');
-        if (i===0) row.append($('<td>').text(""));
-        else row.append($('<td>').text(i)); //first number of each row
-        if (i === 0) {
-            //row.append($('<td>').text('hi'));
-            for (var j = 1; j <= input; j++) { //if in top row, list numbers list on left side
-
-                row.append($('<td class="cell1">').text(j));
-            }
-        }
-        else {
-            for (var j = 1; j <= input; j++) { //do if non-top row
-                if (counter%2==1)
-                    row.append($('<td class="cell2">').text(i*j)); //these ones should have alternating cell color
-                else
-                    row.append($('<td class="cell3">').text(i*j));
-                counter += 1;
-            }
+$(document).keydown(function(keyPressed) {
+    if (keyPressed.keyCode === 13) {
+        if (userInput !== null) {
+            console.log("test123: " + userInput);
         }
 
-        //if i === 0,
-        // add empty cell
-        //else:
-        //  do what you gotta do
-        table.append(row);
+    }
+});
+
+
+$(document).ready(function () {
+    $("#pushbutton").on("click", function () {
+        var inputVal = $(".test").val();
+        if (parseInt(inputVal) > 25 || parseInt(inputVal) <= 0){
+            alert("Please enter a valid positive number less than 25");
+            return;
+        }
+        var input = parseInt(inputVal);
+        makeTable(input);
+    });
+
+    function makeTable(input) {
+        //clear old table
+        $("#here_table").empty();
+
+        var table = $('<table>').addClass('mul_table');
+        var counter = 1;
+        var randomCounter = getRandomNumber(1, input * input);
+
+        for (var i = 0; i <= input; i++) { // rows (i) start at 0
+            var row = $('<tr>').addClass('cell1');
+            if (i === 0) row.append($('<td>').text(""));
+            else row.append($('<td>').text(i)); // first number of each row
+
+            if (i === 0) {
+                for (var j = 1; j <= input; j++) { //if in top row, list numbers on left side
+                    row.append($('<td class="cell1">').text(j));
+                }
+            } else {
+                for (var j = 1; j <= input; j++) { // do if non-top row
+                    if (counter === randomCounter) {
+                        correctAnswer = i*j
+                        // for a randomly selected cell count, adds an input field
+                        var cell;
+                        //var userInput;
+                        var inputVal = $('<input type="text">').on('click', function () {
+                            userInput = $(this).val();
+                            console.log("User entered:", userInput);
+                        });
+
+                        cell = $('<td class="cell_input">').append(inputVal);
+                        row.append(cell);
+                    } else if (counter % 2 === 1) {
+                        row.append($('<td class="cell2">').text(i * j)); // these ones should have alternating cell color
+                    } else {
+                        row.append($('<td class="cell3">').text(i * j));
+                    }
+                    counter += 1;
+                }
+            }
+
+            table.append(row);
+        }
+
+        $('#here_table').append(table);
     }
 
-
-    $('#here_table').append(table);
-
-    nums = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+    var nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
     var counter = 10;
     var autoFlag = false;
     var timer;
-    $("button").on("click", function(){
-        counter = $("input").val();
-        if (counter in nums){
-            if(autoFlag){
+
+    $("button").on("click", function () {
+        counter = parseInt($("input").val()); // ensure numeric value
+        if (nums.includes(counter)) { // check if counter is in nums array
+            if (autoFlag) {
                 autoFlag = false;
                 clearTimeout(timer);
-            }
-            else{
+            } else {
                 autoFlag = true;
                 autoCount();
             }
-
         }
     });
 
-
-    function autoCount(){
-        if (counter >0){
+    function autoCount() {
+        if (counter > 0) {
             console.log("test");
-            counter = counter-1;
-            $("p").html("timer left: " + counter); //the timer text
-            if(autoFlag)
-                timer = setTimeout(autoCount,1000);
+            counter = counter - 1;
+            $("p").html("timer left: " + counter); // the timer text
+            console.log("test user input:", userInput);
+            if (autoFlag) {
+                timer = setTimeout(autoCount, 1000);
+            }
+        } else {
+            $("p").html("Time up!");
+            $("#here_table").empty();
+            $("h1").empty();
+            if (parseInt(userInput) === correctAnswer){
+                $("p").html("Your answer is correct!");
+                console.log("User final input:", userInput);}
+            else
+                $("p").html("Incorrect! The correct answer is: " + correctAnswer);
+                console.log("User final input:", userInput);
+
         }
-        else $("p").html("Time up!");
-
-
-
-
     }
 
-
-
-
-})
-
-
-
-
+    function getRandomNumber(min, max) {
+        min = Math.ceil(min); // round up to nearest integer
+        max = Math.floor(max); // round down to nearest integer
+        var range = max - min;
+        // a random number within the given range
+        return Math.floor(Math.random() * range) + min;
+    }
+});
